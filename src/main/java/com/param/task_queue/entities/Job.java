875197e93fbd31a3791a7b5c728e992b0bd6da02
v4der-248 <vaderskywalker19@gmail.com>
2026-07-13 21:43:22 +0530

    Added proper annotations to Job Class fields along with a JPA Database URL in local

--- src/main/java/com/param/task_queue/entities/Job.java
+++ src/main/java/com/param/task_queue/entities/Job.java
@@ -6,16 +6,27 @@ import java.util.UUID;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
+@Table(name = "jobs")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Job {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Enumerated(EnumType.STRING)
@@ -35,4 +46,15 @@ public class Job {
     private Instant createdAt;
 
     private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = Instant.now();
+    }
 }
